abd9993d7ab1864e0436800485c7f163eabb84d1
Andrey Salnikov <kvanta13@yandex.ru>
2022-03-03 23:30:33 +0300

    Добавьте файлы проекта.

---
+++ UserElem/ColorPicker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserElem
+{
+    public partial class ColorPicker : System.Windows.Controls.UserControl
+    {
+    }
+}
